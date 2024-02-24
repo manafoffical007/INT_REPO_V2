d68b64a6ba90f88799a60cc128a44649544441a9
RolexPc <157006183+RolexPc@users.noreply.github.com>
2024-02-25 00:54:26 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /FILE-STOR-V1
 fi
-cd /DQTheFileDonorBot
+cd /FILE-STOR-V1
 pip3 install -U -r requirements.txt
 echo "Starting FILE-STOR-V1...."
 python3 bot.py
