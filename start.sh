45cbe9b0e1aea15552cefdedd827d87d7e068b6b
RolexPc <157006183+RolexPc@users.noreply.github.com>
2024-02-25 01:11:47 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/RolexPc/FILE-STOR-V1 /FileStorebotv1
+  git clone https://github.com/RolexPc/FileStorebotv1 /FileStorebotv1
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /FileStorebotv1
