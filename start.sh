0481189598cdfbbd7c38d0513eb6a7fe83d9dd75
RolexPc <157006183+RolexPc@users.noreply.github.com>
2024-02-25 01:11:21 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/RolexPc/FILE-STOR-V1 /DQTheFileDonorBot
+  git clone https://github.com/RolexPc/FILE-STOR-V1 /FileStorebotv1
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /DQTheFileDonorBot
+  git clone $UPSTREAM_REPO /FileStorebotv1
 fi
-cd /DQTheFileDonorBot
+cd /FileStorebotv1
 pip3 install -U -r requirements.txt
 echo "Starting DQ-The-File-Donor...."
 python3 bot.py
