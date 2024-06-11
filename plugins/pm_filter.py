71526ef8360b85273386db4dd4db5c9d140596d5
RolexPc <157006183+RolexPc@users.noreply.github.com>
2024-06-11 16:19:08 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -36,7 +36,7 @@ logger.setLevel(logging.ERROR)
 
 BUTTONS = {}
 SPELL_CHECK = {}
-unwanted_words = ['pm', 'kittyo', 'dm', 'me', 'kittiyo', 'msg', 'message', '🔞', 'myre']
+unwanted_words = ['pm', 'kittyo', 'dm', 'me', 'kittiyo', 'msg', 'message', '🔞', 'myre', 'Hy']
 
 @Client.on_message(filters.group)
 async def delete_message(client, message):
