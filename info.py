7832b75ccaaf1a4f9b070de6b3825212029888b0
RolexPc <157006183+RolexPc@users.noreply.github.com>
2024-06-11 17:37:50 +0530

    Update info.py

--- info.py
+++ info.py
@@ -34,6 +34,8 @@ auth_users = [int(user) if id_pattern.search(user) else user for user in environ
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
 auth_grp = environ.get('AUTH_GROUP')
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
+SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(support_chat_id) else None
+support_chat_id = environ.get('SUPPORT_CHAT_ID')
 
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Autofilterv7:Autofilterv7@cluster0.t5tqe4s.mongodb.net/")
